ba3dab04393835f6e3a8aa16e95939834a05f766
AleksandrPetrov89 <petrovav89@gmail.com>
2023-06-21 23:50:31 +0500

    Task 1 added a second solution

--- popups/task.js
+++ popups/task.js
@@ -1,21 +1,45 @@
+// Общая часть для 2-х вариантов
 const modalMain = document.getElementById("modal_main");
 modalMain.className += " modal_active";
+
+// Вариант 2. Он ближе к теме занятия
+// const showSuccess = document.querySelector(".show-success");
+// modalCloseTimes = document.querySelectorAll(".modal__close_times");
+// for (modalClose of modalCloseTimes) {
+//     modalClose.onclick = windowСlosing;
+// };
+
+// showSuccess.onclick = (event) => {
+//     document.getElementById("modal_success").className += " modal_active";
+//     windowСlosing(event);
+// };
+
+
+// function windowСlosing(event) {
+//     let element = event.target.closest(".modal");
+//     element.className = element.className.replace(" modal_active", "");
+//     event.target.onclick = null;
+// };
+
+
+// Вариант 1. На мой взгляд он проще и лучше :)
 modalMain.addEventListener('click', windowHandler);
 
 const modalSuccess = document.getElementById("modal_success");
-modalSuccess.addEventListener('click', windowHandler);
 
 
 function windowHandler(event) {
     // Делегированный обработчик события 'click' для окон "modal_main" и "modal_success".
     // При нажатии на кнопку закрытия окна - делает окно невидимым и удаляет приемник
     // события для этого окна. При нажатии на кнопку "Сделать хорошо" совершает те же действия,
-    // что и при закрытии + делает видимым окно с кнопкой "Хорошо сделано!".
+    // что и при закрытии + делает видимым окно с кнопкой "Хорошо сделано!" и устанавливает на 
+    // него приемник события.
     let element = event.target;
     let modalElement = event.currentTarget;
-    if (element.className.includes("show-success") || element.className.includes("modal__close")) {
+    if (element.className.includes("show-success") || element.className.includes("modal__close_times")) {
         if (element.className.includes("show-success")) {
-            document.getElementById("modal_success").className += " modal_active";
+            modalSuccess.className += " modal_active";
+            modalSuccess.addEventListener('click', windowHandler);
         };
         modalElement.className = modalElement.className.replace(" modal_active", "");
         modalElement.removeEventListener('click', windowHandler);
