0f6b97316ba1de2e2975e4cdf2363a665a739323
AleksandrPetrov89 <petrovav89@gmail.com>
2023-06-28 19:38:22 +0500

    Task 2 (Navigation menu) fixed

--- menu/task.js
+++ menu/task.js
@@ -1,82 +1,28 @@
-// Общая часть
-const allMenuLinks = document.querySelectorAll(".menu__link");
-const allMenuLinksWithSubmenus = [];
+const mainMenus = document.getElementsByClassName("menu_main");
+for (menuMain of mainMenus) {
+    menuMain.addEventListener("click", menuHandler);
+};
 
 
-function hideSubmenu() {
-    // Закрывает все подменю
-    for (let linksWithSubmenus of allMenuLinksWithSubmenus) {
-        if (linksWithSubmenus.parentElement.querySelector(".menu_active")) {
-            linksWithSubmenus.parentElement.querySelector(".menu_active").className = "menu menu_sub";
+function menuHandler (event) {
+    // Делегированный обработчик события при нажатии на пункты меню. 
+    // При нажатии на пункт меню, который содержит подменю, появится подменю. 
+    // При повторном нажатии на пункт меню, содержащий подменю - оно скроется.
+    // При нажатии на пункты меню, которые не содержат подменю, произойдет переход по ссылке.
+    if (event.target.className === "menu__link") {
+        let menuItem = event.target.parentElement;
+        if (menuItem.querySelector(".menu_sub")) {
+            event.preventDefault();
+            if (menuItem.querySelector(".menu_active")) {
+                menuItem.querySelector(".menu_active").className = "menu menu_sub";
+            } else {
+                let main = event.currentTarget;
+                if (main.querySelector(".menu_active")) {
+                    main.querySelector(".menu_active").className = "menu menu_sub";
+                };
+                menuItem.querySelector(".menu_sub").className = "menu menu_sub menu_active";
+            };
+
         };
-    };
-};
-
-
-// Вариант 2
-// for (let link of allMenuLinks) {
-//     link.onclick = menuHandler;
-//     if (link.parentElement.querySelector(".menu_sub")) {
-//         allMenuLinksWithSubmenus.push(link);
-//     };
-// };
-
-
-// function menuHandler(event) {
-//     // Функция обработчик события при нажатии на пункты меню. При нажатии на пункт меню, который содержит 
-//     // подменю, появится подменю. При нажатии на пункты меню, которые не содержат подменю, произойдет 
-//     // переход по ссылке.
-//     let element = event.target;
-
-//     // Не совсем понял совет № 2 "Для вложенных меню вы обязаны возвращать false в обработчиках события
-//     // для предотвращения перехода по ссылке." Если имеется в виду, что переход по ссылкам в подменю
-//     // не должен работать, то закомментированный код ниже это реализует (хотя как то странно было бы)
- 
-//     // if (element.closest(".menu_sub")) {
-//     //     return false;
-//     // };
-
-//     hideSubmenu();
-//     if (allMenuLinksWithSubmenus.includes(element)) {
-//         element.parentElement.querySelector(".menu_sub").className += " menu_active";
-//         return false;
-//     };
-// };
-
-
-// Вариант 1 с делегированным обработчиком событий
-for (let link of allMenuLinks) {
-    if (link.parentElement.querySelector(".menu_sub")) {
-        allMenuLinksWithSubmenus.push(link);
-    };
-};
-const body = document.querySelector("body");
-body.addEventListener('click', (event) => {
-    if (["menu__link", "logo__link"].includes(event.target.className)) {
-        menuHandler(event);
-    } else {
-        hideSubmenu();
-    };
-});
-
-
-function menuHandler(event) {
-    // Функция обработчик события при нажатии на пункты меню. При нажатии на пункт меню, который содержит 
-    // подменю, появится подменю. При нажатии на пункты меню, которые не содержат подменю, произойдет 
-    // переход по ссылке. При нажатии на любые другие элементы страницы происходит закрытие всех подменю.
-    let element = event.target;
-
-    // Не совсем понял совет № 2 "Для вложенных меню вы обязаны возвращать false в обработчиках события
-    // для предотвращения перехода по ссылке." Если имеется в виду, что переход по ссылкам в подменю
-    // не должен работать, то закомментированный код ниже это реализует (хотя как то странно было бы)
- 
-    // if (element.closest(".menu_sub")) {
-    //     return false;
-    // };
-
-    hideSubmenu();
-    if (allMenuLinksWithSubmenus.includes(element)) {
-        event.preventDefault();
-        element.parentElement.querySelector(".menu_sub").className += " menu_active";
     };
 };
